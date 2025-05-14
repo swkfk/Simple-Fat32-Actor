6058af8ce16f43dbe7dcf932fd60adcf50ba06cb
kai_Ker <kai_Ker@buaa.edu.cn>
2025-05-14 21:22:59 +0800

    Fix the wrong flags in fat entry

--- fat32/common.h
+++ fat32/common.h
@@ -9,10 +9,10 @@
 #define ReservedArrayField(bytes, name) uint8_t _##name[bytes];
 
 #define FAT_ENTRY_VALID_LOW 0x00000002
-#define FAT_ENTRY_VALID_HIGH (0x0FFFFFFE + 1)
+#define FAT_ENTRY_VALID_HIGH (0x0FFFFFEF + 1)
 #define FAT_ENTRY_TERMINATE_LOW 0x0FFFFFF8
 #define FAT_ENTRY_TERMINATE_HIGH 0x0FFFFFFF + 1
-#define FAT_ENTRY_BAD 0xFFFFFFF7
+#define FAT_ENTRY_BAD 0x0FFFFFF7
 
 typedef uint32_t fat_entry_t;
 
